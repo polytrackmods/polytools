6db9fbc66cc6aa70f674b51329da040d8dfeb1d3
Ireozar <ireo@ireo.dev>
2025-02-16 16:30:14 +0100

    added hof leaderboard to polyweb

--- polyweb/src/main.rs
+++ polyweb/src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+use rocket::form::validate::Len;
 use rocket::fs::FileServer;
 use rocket::futures::future::join_all;
 use rocket::serde::{Deserialize, Serialize};
@@ -28,20 +29,33 @@ struct LeaderBoard {
 #[derive(Serialize, Deserialize)]
 struct Entry {
     rank: u32,
-    time: String,
+    stat: String,
     name: String,
 }
 
 const BLACKLIST_FILE: &str = "blacklist.txt";
 const ALT_ACCOUNT_FILE: &str = "alt_accounts.txt";
-const RANKINGS_FILE: &str = "poly_rankings.txt";
+const GLOBAL_RANKINGS_FILE: &str = "poly_rankings.txt";
+const HOF_RANKINGS_FILE: &str = "hof_rankings.txt";
 const MAX_RANKINGS_AGE: Duration = Duration::from_secs(60 * 10);
 const AUTOUPDATE_TIMER: Duration = Duration::from_secs(60 * 30);
 
 #[get("/")]
 async fn index() -> Template {
-    let leaderboard = parse_leaderboard(RANKINGS_FILE).await;
-    Template::render("index", context! { leaderboard })
+    let leaderboard = parse_leaderboard(GLOBAL_RANKINGS_FILE).await;
+    Template::render(
+        "index",
+        context! { title: "Global Leaderboard", stat: "Time", leaderboard },
+    )
+}
+
+#[get("/hof")]
+async fn hof() -> Template {
+    let leaderboard = parse_leaderboard(HOF_RANKINGS_FILE).await;
+    Template::render(
+        "index",
+        context! { title: "HOF Leaderboard", stat: "Points", leaderboard },
+    )
 }
 
 #[get("/tutorial")]
@@ -53,13 +67,13 @@ async fn tutorial() -> Template {
 #[main]
 async fn main() -> Result<(), rocket::Error> {
     let rocket = rocket::build()
-        .mount("/", routes![index, tutorial])
+        .mount("/", routes![index, hof, tutorial])
         .mount("/static", FileServer::from("static"))
         .attach(Template::fairing());
     task::spawn(async {
         loop {
-            if tokio::fs::try_exists(RANKINGS_FILE).await.unwrap() {
-                let age = tokio::fs::metadata(RANKINGS_FILE)
+            /* if tokio::fs::try_exists(GLOBAL_RANKINGS_FILE).await.unwrap() {
+                let age = tokio::fs::metadata(GLOBAL_RANKINGS_FILE)
                     .await
                     .unwrap()
                     .modified()
@@ -71,6 +85,20 @@ async fn main() -> Result<(), rocket::Error> {
                 }
             } else {
                 rankings_update().await.expect("Failed update");
+            } */
+            if tokio::fs::try_exists(HOF_RANKINGS_FILE).await.unwrap() {
+                let age = tokio::fs::metadata(HOF_RANKINGS_FILE)
+                    .await
+                    .unwrap()
+                    .modified()
+                    .unwrap()
+                    .elapsed()
+                    .unwrap();
+                if age > MAX_RANKINGS_AGE {
+                    hof_update().await.expect("Failed update");
+                }
+            } else {
+                hof_update().await.expect("Failed update");
             }
             sleep(AUTOUPDATE_TIMER).await;
         }
@@ -94,7 +122,7 @@ async fn parse_leaderboard(file_path: &str) -> Vec<Entry> {
             if parts.len() == 3 {
                 Some(Entry {
                     rank: parts[0].parse().ok()?,
-                    time: parts[1].to_string(),
+                    stat: parts[1].to_string(),
                     name: parts[2].to_string(),
                 })
             } else {
@@ -214,6 +242,125 @@ async fn rankings_update() -> Result<(), Error> {
             .as_str(),
         );
     }
-    tokio::fs::write(RANKINGS_FILE, output.clone()).await?;
+    tokio::fs::write(GLOBAL_RANKINGS_FILE, output.clone()).await?;
+    Ok(())
+}
+
+async fn hof_update() -> Result<(), Error> {
+    dotenv::dotenv().ok();
+    let id = env::var("LEADERBOARD_ID").expect("Expected OWNER_ID in env!");
+    let client = reqwest::Client::new();
+    let track_ids: Vec<String> = tokio::fs::read_to_string("hof_tracks.txt")
+        .await?
+        .lines()
+        .map(|s| s.to_string())
+        .collect();
+    let track_num = track_ids.len() as u32;
+    let futures = track_ids.into_iter().map(|track_id| {
+        let client = client.clone();
+        let url = format!("https://vps.kodub.com:43273/leaderboard?version=0.4.0&trackId={}&skip=0&amount=100&onlyVerified=false&userTokenHash={}",
+            track_id.splitn(2, " ").next().unwrap(),
+            id);
+        task::spawn(
+            async move {
+                let res = client.get(url).send().await.unwrap().text().await.unwrap();
+                return Ok::<String, reqwest::Error>(res);
+            })
+        });
+    let results: Vec<String> = join_all(futures)
+        .await
+        .into_iter()
+        .map(|res| res.unwrap())
+        .filter_map(|res| res.ok())
+        .collect();
+    let mut leaderboards: Vec<Vec<LeaderBoardEntry>> = Vec::new();
+    for result in results {
+        let leaderboard: Vec<LeaderBoardEntry> =
+            serde_json::from_str::<LeaderBoard>(&result)?.entries;
+        leaderboards.push(leaderboard);
+    }
+    let mut player_rankings: HashMap<String, Vec<usize>> = HashMap::new();
+    let blacklist: Vec<String> = tokio::fs::read_to_string(BLACKLIST_FILE)
+        .await?
+        .lines()
+        .map(|s| s.to_string())
+        .collect();
+    let alt_file: Vec<String> = tokio::fs::read_to_string(ALT_ACCOUNT_FILE)
+        .await?
+        .lines()
+        .map(|s| s.to_string())
+        .collect();
+    let mut alt_list: HashMap<String, String> = HashMap::new();
+    for line in alt_file {
+        const SPLIT_CHAR: &str = "<|>";
+        for entry in line.split(SPLIT_CHAR).skip(1) {
+            alt_list.insert(
+                entry.to_string(),
+                line.split(SPLIT_CHAR).nth(0).unwrap().to_string(),
+            );
+        }
+    }
+    let point_values: Vec<u32> = fs::read_to_string("hof_points.txt")
+        .await?
+        .lines()
+        .map(|s| s.to_string().parse().unwrap())
+        .collect();
+    for leaderboard in leaderboards {
+        let mut has_ranking: Vec<String> = Vec::new();
+        let mut pos = 0;
+        for entry in leaderboard {
+            if pos + 1 > point_values.len() {
+                break;
+            }
+            let name;
+            if alt_list.contains_key(&entry.name) {
+                name = alt_list.get(&entry.name).unwrap().clone();
+            } else {
+                name = entry.name.clone();
+            }
+            if !has_ranking.contains(&name) && !blacklist.contains(&name) {
+                player_rankings
+                    .entry(name.clone())
+                    .or_insert(Vec::new())
+                    .push(pos);
+                has_ranking.push(name);
+                pos += 1;
+            }
+        }
+    }
+    let mut sorted_leaderboard: Vec<(String, u32)> = player_rankings
+        .into_iter()
+        .map(|(name, rankings)| {
+            let mut points = 0;
+            for ranking in rankings {
+                if ranking < point_values.len() {
+                    points += point_values[ranking];
+                }
+            }
+            (name, points)
+        })
+        .collect();
+    sorted_leaderboard.sort_by_key(|(_, ranking)| *ranking);
+    sorted_leaderboard.reverse();
+    let mut final_leaderboard: Vec<(u32, u32, String)> = Vec::new();
+    let mut points_prev = point_values[0] * track_num + 1;
+    let mut rank_prev = 0;
+    for entry in sorted_leaderboard.clone() {
+        if entry.1 < points_prev {
+            points_prev = entry.1;
+            rank_prev += 1;
+        }
+        final_leaderboard.push((rank_prev, points_prev, entry.0));
+    }
+    /* let leaderboard: Vec<(usize, String, u32)> = sorted_leaderboard
+        .into_iter()
+        .enumerate()
+        .map(|(i, (name, ranking))| (i, name, ranking))
+        .collect(); */
+    let mut output = String::new();
+    for entry in final_leaderboard {
+        output.push_str(format!("{:>3} - {} - {}\n", entry.0, entry.1, entry.2,).as_str());
+    }
+    tokio::fs::write(HOF_RANKINGS_FILE, output.clone()).await?;
     Ok(())
 }
