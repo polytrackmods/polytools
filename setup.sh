f1a3f511294bbd20ad042d3e8ed38b4e11e58328
Ireozar <ireo@ireo.dev>
2025-03-13 09:33:28 +0100

    added diesel setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 touch data/blacklist.txt
 touch data/alt_accounts.txt
 touch data/poly_rankings.txt
-
 touch data/hof_blacklist.txt
 touch data/hof_alt_accounts.txt
 touch data/hof_rankings.txt
-
 touch data/0.5_poly_rankings.txt
 touch data/custom_tracks.txt
 
 cat .env &> /dev/null || cp .env.example .env
 
 touch templates/privacy_policy.html.tera
-echo "Please setup a Privacy Policy in templates/privacy_policy.html.tera"
+echo "Add your Privacy Policy in templates/privacy_policy.html.tera"
+
+diesel migration run
