b99f551d3ed7621f85d741cad06fc01508dcae55
Ireozar <ireo@ireo.xyz>
2025-04-15 16:05:03 +0200

    added /records

--- polytracker/src/main.rs
+++ polytracker/src/main.rs
@@ -1,6 +1,7 @@
 mod commands;
 pub mod utils;
 
+use commands::records;
 use commands::{
     assign, compare, delete, edit_lists, help, list, players, policy, rankings, request,
     update_rankings, users,
@@ -51,6 +52,7 @@ async fn main() {
                 help(),
                 compare(),
                 update_rankings(),
+                records(),
                 rankings(),
                 policy(),
             ],
