1aed1a68be0c9de6db8f834d21e71e4f5ef429a8
Ireozar <ireo@ireo.xyz>
2025-05-06 13:02:10 +0200

    added very simple API

---
+++ polyweb/src/api.rs
@@ -0,0 +1,42 @@
+use polymanager::{
+    COMMUNITY_RANKINGS_FILE, COMMUNITY_TIME_RANKINGS_FILE, HOF_RANKINGS_FILE,
+    HOF_TIME_RANKINGS_FILE, RANKINGS_FILE,
+};
+use rocket::{get, request::FromParam, tokio::fs};
+
+pub enum ApiList {
+    Global,
+    Hof,
+    HofTime,
+    Community,
+    CommunityTime,
+}
+impl<'a> FromParam<'a> for ApiList {
+    type Error = &'a str;
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        use ApiList::*;
+        match param.to_lowercase().as_str() {
+            "global" => Ok(Global),
+            "hof" => Ok(Hof),
+            "hof_time" => Ok(HofTime),
+            "community" => Ok(Community),
+            "community_time" => Ok(CommunityTime),
+            _ => Err("Failed to find enum"),
+        }
+    }
+}
+
+#[get("/api/<list>")]
+pub async fn get_api(list: ApiList) -> String {
+    let file = {
+        use ApiList::*;
+        match list {
+            Global => RANKINGS_FILE,
+            Hof => HOF_RANKINGS_FILE,
+            HofTime => HOF_TIME_RANKINGS_FILE,
+            Community => COMMUNITY_RANKINGS_FILE,
+            CommunityTime => COMMUNITY_TIME_RANKINGS_FILE,
+        }
+    };
+    fs::read_to_string(file).await.unwrap()
+}
