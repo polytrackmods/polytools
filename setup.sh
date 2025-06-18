aaf9e4957ffaace97d9b56ed3f5e0431eebb18be
Ireozar <ireo@ireo.xyz>
2025-06-18 14:21:11 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-touch data/blacklist.txt
 touch data/alt_accounts.txt
-touch data/poly_rankings.txt
-touch data/hof_blacklist.txt
+touch data/blacklist.txt
+touch data/community_rankings.txt
+touch data/community_time_rankings.txt
+touch data/custom_tracks.txt
 touch data/hof_alt_accounts.txt
+touch data/hof_blacklist.txt
 touch data/hof_rankings.txt
 touch data/hof_time_rankings.txt
-touch data/custom_tracks.txt
+touch data/poly_rankings.txt
 
 test -e .env || cp .env.example .env
 
