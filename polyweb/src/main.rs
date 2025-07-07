ccc625c3c72dae6a95b4cfc3e4214480d0e040ba
Ireozar <ireo@ireo.xyz>
2025-07-07 14:00:50 +0200

    formatting

--- polyweb/src/main.rs
+++ polyweb/src/main.rs
@@ -8,7 +8,9 @@ use parsers::{
     parse_leaderboard_with_records,
 };
 use polymanager::{
-    community_update, et_rankings_update, global_rankings_update, hof_update, COMMUNITY_RANKINGS_FILE, COMMUNITY_TRACK_FILE, CUSTOM_TRACK_FILE, HOF_RANKINGS_FILE, RANKINGS_FILE, TRACK_FILE, UPDATE_CYCLE_LEN, UPDATE_LB_COUNT
+    community_update, et_rankings_update, global_rankings_update, hof_update,
+    COMMUNITY_RANKINGS_FILE, COMMUNITY_TRACK_FILE, CUSTOM_TRACK_FILE, HOF_RANKINGS_FILE,
+    RANKINGS_FILE, TRACK_FILE, UPDATE_CYCLE_LEN, UPDATE_LB_COUNT,
 };
 use rocket::form::Context;
 use rocket::fs::FileServer;
